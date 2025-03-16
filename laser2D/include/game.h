107011c502a89d059f7b6c9243fbbddc477aea3f
Carvalho Dario Adolfo <kae@MacBook-Pro-de-Carvalho.local>
2025-03-16 02:26:22 +0100

    Displaying star and split run in draw and update

--- laser2D/include/game.h
+++ laser2D/include/game.h
@@ -2,13 +2,19 @@
 #include "raylib.h"
 #include "GameSettings.h"
 #include "player.h"
+#include <vector>
 class Game
 {
 private:
    Player *player;
-
+   std::vector<std::pair<Vector2, float>> starData;
+   Texture2D starTexture;
+   void generateStartData();
 public:
    Game();
    ~Game();
    void run();
+   void drawStar();
+   void update();
+   void draw();
 };
--- laser2D/src/game.cpp
+++ laser2D/src/game.cpp
@@ -1,27 +1,60 @@
 #include "game.h"
 #include <stdio.h>
+#include <cstdlib>
 
 Game::Game()
 {
     InitWindow(GameSettings::WINDOW_WIDTH, GameSettings::WINDOW_HEIGHT, "Space ship");
+    generateStartData();
     player = new Player(LoadTexture("images/spaceship.png"), Vector2{GameSettings::WINDOW_WIDTH / 2, GameSettings::WINDOW_HEIGHT / 2});
+    starTexture = LoadTexture("images/star.png");
+}
+
+void Game::generateStartData()
+{
+    for (int i = 0; i < 30; i++)
+    {
+        Vector2 pos = {static_cast<float>(rand() % GameSettings::WINDOW_WIDTH), static_cast<float>(rand() % GameSettings::WINDOW_HEIGHT)};
+        float size = 0.5f + static_cast<float>(rand()) / RAND_MAX * (1.8f - 0.5f); // Random float between 0.5 and 1.6
+        starData.emplace_back(pos, size);
+    }
+}
+
+void Game::drawStar()
+{
+    for (const auto &starData : starData)
+    {
+        DrawTextureEx(starTexture, starData.first, 0, starData.second, WHITE);
+    }
+}
+
+void Game::update()
+{
+    float dt = GetFrameTime();
+    player->update(dt);
+}
+
+void Game::draw()
+{
+    BeginDrawing();
+        ClearBackground({15, 10, 25, 255});
+        drawStar();
+        player->draw();
+    EndDrawing();
 }
 
 void Game::run()
 {
     while (!WindowShouldClose())
     {
-        float dt = GetFrameTime();
-        player->update(dt);
-        BeginDrawing();
-        ClearBackground({15, 10, 25, 255});
-        player->draw();
-        EndDrawing();
+        update();
+        draw();
     }
     CloseWindow();
 }
 
 Game::~Game()
 {
+    UnloadTexture(starTexture);
     delete player;
 }
